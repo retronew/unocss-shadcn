666243e3b23bd9b0a2332698da154d688bccd86b
retronew <7346148+retronew@users.noreply.github.com>
2025-06-17 17:07:02 +0800

    feat: removed useless border radius

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 import { definePreset } from '@unocss/core'
 
+export interface Colors {
+  [key: string]: Colors & { DEFAULT?: string } | string
+}
+
+interface Theme {
+  colors?: Colors
+}
+
 function getShadcnThemeCSS() {
   return `
   :root {
@@ -95,14 +103,75 @@ function getShadcnThemeCSS() {
 
   * {
     border-color: var(--border);
-    outline-color: var(--ring) / 50%);
+    outline-color: color-mix(in oklch, var(--ring) 50%, transparent);
   }
-
   body {
+    background-color: var(--background);
     color: var(--foreground);
-    background: var(--background);
   }
 `
+}
+
+function getTheme(): Theme {
+  return {
+    colors: {
+      border: 'var(--border)',
+      input: 'var(--input)',
+      ring: 'var(--ring)',
+      background: 'var(--background)',
+      foreground: 'var(--foreground)',
+      primary: {
+        DEFAULT: 'var(--primary)',
+        foreground: 'var(--primary-foreground)',
+      },
+      secondary: {
+        DEFAULT: 'var(--secondary)',
+        foreground: 'var(--secondary-foreground)',
+      },
+      destructive: {
+        DEFAULT: 'var(--destructive)',
+        foreground: 'var(--destructive-foreground)',
+      },
+      muted: {
+        DEFAULT: 'var(--muted)',
+        foreground: 'var(--muted-foreground)',
+      },
+      accent: {
+        DEFAULT: 'var(--accent)',
+        foreground: 'var(--accent-foreground)',
+      },
+      popover: {
+        DEFAULT: 'var(--popover)',
+        foreground: 'var(--popover-foreground)',
+      },
+      card: {
+        DEFAULT: 'var(--card)',
+        foreground: 'var(--card-foreground)',
+      },
+      chart: {
+        1: 'var(--chart-1)',
+        2: 'var(--chart-2)',
+        3: 'var(--chart-3)',
+        4: 'var(--chart-4)',
+        5: 'var(--chart-5)',
+      },
+      sidebar: {
+        DEFAULT: 'var(--sidebar)',
+        foreground: 'var(--sidebar-foreground)',
+        background: 'var(--sidebar-background)',
+        primary: {
+          DEFAULT: 'var(--sidebar-primary)',
+          foreground: 'var(--sidebar-primary-foreground)',
+        },
+        accent: {
+          DEFAULT: 'var(--sidebar-accent)',
+          foreground: 'var(--sidebar-accent-foreground)',
+        },
+        border: 'var(--sidebar-border)',
+        ring: 'var(--sidebar-ring)',
+      },
+    },
+  }
 }
 
 export const presetShadcn = definePreset(() => {
@@ -113,70 +182,6 @@ export const presetShadcn = definePreset(() => {
         getCSS: getShadcnThemeCSS,
       },
     ],
-    theme: {
-      colors: {
-        border: 'var(--border)',
-        input: 'var(--input)',
-        ring: 'var(--ring)',
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
-        primary: {
-          DEFAULT: 'var(--primary)',
-          foreground: 'var(--primary-foreground)',
-        },
-        secondary: {
-          DEFAULT: 'var(--secondary)',
-          foreground: 'var(--secondary-foreground)',
-        },
-        destructive: {
-          DEFAULT: 'var(--destructive)',
-          foreground: 'var(--destructive-foreground)',
-        },
-        muted: {
-          DEFAULT: 'var(--muted)',
-          foreground: 'var(--muted-foreground)',
-        },
-        accent: {
-          DEFAULT: 'var(--accent)',
-          foreground: 'var(--accent-foreground)',
-        },
-        popover: {
-          DEFAULT: 'var(--popover)',
-          foreground: 'var(--popover-foreground)',
-        },
-        card: {
-          DEFAULT: 'var(--card)',
-          foreground: 'var(--card-foreground)',
-        },
-        chart: {
-          1: 'var(--chart-1)',
-          2: 'var(--chart-2)',
-          3: 'var(--chart-3)',
-          4: 'var(--chart-4)',
-          5: 'var(--chart-5)',
-        },
-        sidebar: {
-          DEFAULT: 'var(--sidebar)',
-          foreground: 'var(--sidebar-foreground)',
-          background: 'var(--sidebar-background)',
-          primary: {
-            DEFAULT: 'var(--sidebar-primary)',
-            foreground: 'var(--sidebar-primary-foreground)',
-          },
-          accent: {
-            DEFAULT: 'var(--sidebar-accent)',
-            foreground: 'var(--sidebar-accent-foreground)',
-          },
-          border: 'var(--sidebar-border)',
-          ring: 'var(--sidebar-ring)',
-        },
-      },
-      borderRadius: {
-        sm: 'calc(var(--radius) - 4px)',
-        md: 'calc(var(--radius) - 2px)',
-        lg: 'var(--radius)',
-        xl: 'calc(var(--radius) + 4px)',
-      },
-    },
+    theme: getTheme(),
   }
 })
